6ac5286cd1f8c028c5b1ebf2fd6745323f6031a4
nzr-vi <96348033+nzr-vi@users.noreply.github.com>
2022-09-04 19:10:14 +0200

    creating base login modal

--- src/main/webapp/commons/script.js
+++ src/main/webapp/commons/script.js
@@ -1,3 +1,50 @@
-/**
- * 
- */
+"use strict";
+
+let username = null;
+let isLoggedIn = false;
+let loginModal = null;
+
+function pageInit(){
+	
+	let loggedInData = document.getElementById("h-i-logged-in");
+	if(isLoggedIn!=null){
+		isLoggedIn = loggedInData.value;
+		
+		console.log(isLoggedIn);
+		
+		if(isLoggedIn == true){
+			username = document.getElementById("h-i-username").value;
+			let welcome_span = document.getElementById("span-welcome-msg");
+		    welcome_span.textContent = "Welcome "+username;
+		}	
+	}
+	
+	initLoginModal();
+	buildLogInOutBtn();
+}
+
+function buildLogInOutBtn(){
+	let logInOutDiv = document.getElementById("div-log-in-out");
+	let url = "", text = "";
+	let new_btn = document.createElement("button");
+	if(isLoggedIn == true){
+		url = "logout";
+		text = "<i class=\"bi bi-unlock-fill\"></i> logout";
+	}
+	else{
+		url = "login";
+		text = "<i class=\"bi bi-lock-fill\"></i> login";
+		new_btn.addEventListener("click", ()=>loginModal.show());
+	}
+	
+	new_btn.classList.add("btn","btn-secondary");
+	new_btn.setAttribute("data-url",url);
+	new_btn.innerHTML=text;
+	logInOutDiv.appendChild(new_btn);
+}
+
+function initLoginModal(){
+	loginModal = new bootstrap.Modal(document.getElementById('login_modal'))
+}
+
+document.addEventListener("DOMContentLoaded", pageInit);
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,3 +1,6 @@
+
+<%@page import="it.unipa.apsw.finalproj.model.User"%>
+<%@page import="it.unipa.apsw.finalproj.controller.LoginCtrl"%>
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html>
@@ -6,11 +9,34 @@
 	<meta charset="ISO-8859-1">
 	<title>CoffeDoooom</title>
 	<%@ include file="/fragments/f_bootstrap_css_import.html" %>
+	<link rel="stylesheet" href="/Distributor/commons/style.css">
 </head>
 <body>
+	<% 
+		String username = "";
+		User user = (User)session.getAttribute(LoginCtrl.LOGGED_USER_KEY);
+		boolean loggedIn = user!=null;
+		if(loggedIn){
+			username = user.getName()+" "+user.getSurname();
+		}
+	%>
+	
+	<%@ include file="/fragments/f_navbar.jsp" %>
+
 	<h1>Hello here</h1>
+	
+
+
+	<%@ include file="/fragments/f_login_modal.html" %>
+	
+	<!-- DATA HIDDEN FORM -->
+	<form action="none" hidden="true">
+		<input type="hidden" id="h-i-logged-in" name="loggedIn" value="<%=loggedIn%>">
+		<input type="hidden" id="h-i-username" name="username" value="<%=username%>">
+	</form>
 	
 	<!-- SCRIPTS -->
 	<%@ include file="/fragments/f_bootstrap_script_import.html" %>
+	<script type="text/javascript" src="/Distributor/commons/script.js"></script>
 </body>
 </html>
